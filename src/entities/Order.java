efdb2f8abd83c4cc470a4707b9535946fffbdcce
vandretorres <vandre.torres@gmail.com>
2020-07-10 09:44:42 -0300

    versão 1.1

---
+++ src/entities/Order.java
@@ -0,0 +1,78 @@
+package entities;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import entities.enums.OrderItem;
+import entities.enums.OrderStatus;
+
+public class Order {
+
+	
+	private Date moment;
+	private OrderStatus status;
+	private Client client;	
+	private List<OrderItem> orderItem = new ArrayList<>();
+	
+	
+	public Order() {
+		
+	}
+
+
+	public Order(Date moment, OrderStatus status, Client client, List<OrderItem> orderItem) {
+		
+		this.moment = moment;
+		this.status = status;
+	}
+
+
+	public Date getMoment() {
+		return moment;
+	}
+
+
+	public void setMoment(Date moment) {
+		this.moment = moment;
+	}
+
+
+	public OrderStatus getStatus() {
+		return status;
+	}
+
+
+	public void setStatus(OrderStatus status) {
+		this.status = status;
+	}
+
+
+	public Client getClient() {
+		return client;
+	}
+
+
+	public void setClient(Client client) {
+		this.client = client;
+	}
+	
+	
+	public void addItem(OrderItem item) {
+		
+		orderItem.add(item);
+		
+	}
+	
+public void removeItem(OrderItem item) {
+		
+		orderItem.remove(item);
+		
+	}
+	
+	
+	
+	
+	
+	
+}
---
+++ src/entities/enums/OrderItem.java
@@ -0,0 +1,56 @@
+package entities.enums;
+
+public class OrderItem {
+	
+	private Product product;
+	
+	private Integer quantity;
+	private Double price;
+	
+	public OrderItem() {
+		
+	}
+
+	public OrderItem(Product product, Integer quantity, Double price) {
+		
+		this.product = product;
+		this.quantity = quantity;
+		this.price = price;
+	}
+
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+
+	public Integer getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+	
+	
+	public Double subTotal() {
+		
+		return quantity * price;
+		
+	}
+	
+	
+	
+	
+
+}
